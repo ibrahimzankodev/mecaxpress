4463b7c39bd17ba3b68cdb131cd35acb354bf276
ibrahimzankodev <ibraimzankodev@gmail.com>
2025-11-24 02:24:10 +0100

    Add Cloudinary and Strapi image domains to Next.js config

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,18 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: process.cwd(),
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'mecaxpress-be-1.onrender.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
